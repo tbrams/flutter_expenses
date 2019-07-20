a9d085b8d997eeb5d38fcf20113cbb42c0567966
tbrams <torben@brams.dk>
2019-07-20 23:48:29 +0200

    Refactoring to components, user transactions

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,9 @@ class MyHomePage extends StatelessWidget {
     ),
   ];
 
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,14 +43,39 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          Card(
-            child: Container(
-              width: double.infinity,
+          Container(
+            width: double.infinity,
+            height: 100,
+            child: Card(
               color: Colors.orange,
-              height: 100,
               child: Text('CHART'),
+              elevation: 5,
             ),
+          ),
+          Card(
             elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    TextField(
+                      controller: titleController,
+                      decoration: InputDecoration(labelText: 'Title'),
+                    ),
+                    TextField(
+                      controller: amountController,
+                      decoration: InputDecoration(labelText: 'Amount'),
+                    ),
+                    FlatButton(
+                        child: Text('Add Transaction'),
+                        textColor: Colors.purple,
+                        onPressed: () {
+                          print(titleController.text);
+                          print(amountController.text);
+                        }),
+                  ]),
+            ),
           ),
           Column(
             children: transactions.map((tx) {
