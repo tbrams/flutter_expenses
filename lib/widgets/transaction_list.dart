430fd8ad8250f9605b9f319220d15054182a3891
tbrams <torben@brams.dk>
2019-08-01 19:50:12 +0200

    Using ListTile instead of the Custom Card Style

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -29,45 +29,16 @@ class TransactionList extends StatelessWidget {
             )
           : ListView.builder(
               itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context).primaryColor,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                            '\$${transactions[index].amount.toStringAsFixed(2)}',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Theme.of(context).primaryColor,
-                            )),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            transactions[index].title,
-                            style: Theme.of(context).textTheme.title,
-                          ),
-                          Text(
-                            DateFormat('yyyy-MM-dd')
-                                .format(transactions[index].date),
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 12,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                return ListTile(
+                  leading: CircleAvatar(
+                      radius: 30,
+                      child: Text('\$${transactions[index].amount}')),
+                  title: Text(
+                    transactions[index].title,
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
                   ),
                 );
               },
