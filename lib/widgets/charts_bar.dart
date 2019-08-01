bcca61efec1d842c51e0ad98ecb0e9cad9c6b3ba
tbrams <torben@brams.dk>
2019-08-01 19:32:44 +0200

    FittedBox only

--- lib/widgets/charts_bar.dart
+++ lib/widgets/charts_bar.dart
@@ -10,7 +10,9 @@ class ChartBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(children: <Widget>[
-      Text('\$${spendingAmount.toStringAsFixed(0)}'),
+      FittedBox(
+        child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+       ),
       SizedBox(height: 4),
       Container(
         height: 60,
@@ -34,7 +36,9 @@ class ChartBar extends StatelessWidget {
           )
         ]),
       ),
-      SizedBox(height: 4),
+      SizedBox(
+        height: 4,
+      ),
       Text(label),
     ]);
   }
